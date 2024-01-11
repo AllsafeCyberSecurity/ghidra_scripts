f15c909e754521fcf8d2187450048cdde28bbfa6
pinksawtooth <cobhc.pinksawtooth@gmail.com>
2024-01-11 15:31:39 +0900

    Update non_zero_xor_search.py

--- non_zero_xor_search.py
+++ non_zero_xor_search.py
@@ -9,25 +9,25 @@ def add_bookmark_comment(addr, text):
 	createBookmark(addr, "non zero xor", text)
 	cu.setComment(CodeUnit.EOL_COMMENT, text)
 
+def getFunctionNameAtAddress(address):
+    function = getFunctionContaining(address)
+    if function is not None:
+        return function.getName()
+    else:
+        return "No function at specified address"
+
 #get all memory ranges
 ranges = currentProgram.getMemory().getAddressRanges()
 print("--------------------------------")
 
-for r in ranges:
-    begin = r.getMinAddress()
-    length = r.getLength()
+instructions = currentProgram.getListing().getInstructions(True)
 
-    ins = getInstructionAt(begin)
-    while(ins==None):
-        ins =  getInstructionAfter(ins)
-    for i in range(length):
+for ins in instructions:
         mnemonic = ins.getMnemonicString()
         if mnemonic == "XOR":
             operand1 = ins.getOpObjects(0)
             operand2 = ins.getOpObjects(1)
             if operand1 != operand2:
-                print("{} {}".format(ins.address, ins))
+                print("{} {} '{}'".format(ins.address,  getFunctionNameAtAddress(ins.address), ins))
                 add_bookmark_comment(ins.address, str(ins))
         ins =  getInstructionAfter(ins)
-        while(ins==None):
-            ins =  getInstructionAfter(ins)
